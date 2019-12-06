a2da4dc3c8c1cba96c43abeae37225c4c9d87d3b
zsozsozs <zso@zso.com>
2019-12-06 15:17:00 +0100

    Work on login&logout logic

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const shuffle = require(__dirname + "/shuffle.js");
+const findPlayer = require(__dirname + "/player.js");
+
 
 // Create a new Express application
 const app = express();
@@ -19,24 +21,84 @@ const server = require('http').createServer(app);
 // BEFORE const io = require('socket.io')(server);
 var io = require('socket.io').listen(server);
 
+// CONSTANTS and VARIABLES
+const letterStack = [];
+let shuffledLetters = [];
+const players = [];
+const letterStackPerPlayer = 7;
+const maxPlayers = 3;
+
 // Listen on port 3000.
 server.listen(3000, function(req, res) {
   console.log("Server running on port 3000.");
 });
 
 io.on('connection', function(socket){
+
+  console.log("SOCKETS at connection start:");
+  console.log(io.sockets.adapter.rooms['family-scrabble']);
+
   console.log('BREAKING: a user connected');
+  console.log("PLAYERS at connect: " + players.length);
   socket.on('disconnect', function(){
+
+    const filteredPlayerIndex = findPlayer.findPlayerFromSocketID(socket.id, players);
+    console.log("DISCONNECTING: " + players[filteredPlayerIndex].name);
+    const loggedOutUser = {
+      index: filteredPlayerIndex,
+      name: players[filteredPlayerIndex].name
+    };
+    console.log("OBJECT loggedOutUser: ");
+    console.log(loggedOutUser);
+    io.in('family-scrabble').emit('player logout', loggedOutUser);
+    players.splice(filteredPlayerIndex, 1);
+    io.in('family-scrabble').emit('update players', players);
+
+    console.log("PLAYERS AFTER REMOVING after DISCONNECT");
+    console.log(players);
+
     console.log('BREAKING: user disconnected');
+    console.log("PLAYERS at disconnect: " + players.length);
   });
-});
 
+  socket.on('player login', function(playerName){
+    console.log('message: ' + playerName);
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    if(players.length <= (maxPlayers-1)){
+      console.log("MAX players: " + maxPlayers);
+      console.log("PLAYERS at start: " + players.length);
+
+      let newPlayer = new Player(playerName, players.length+1, socket.id);
+      players.push(newPlayer);
+
+      // join game room
+      socket.join("family-scrabble");
+      //  notify everyone (except user logging in) of new player in game room
+      socket.to('family-scrabble').emit('player login', playerName);
+      // update everyone's player list
+      io.in('family-scrabble').emit('update players', players);
+
+      console.log("PLAYERS after adding player: " + players.length);
+      console.log("ROOM MEMBERS after adding player: " + io.sockets.adapter.rooms['family-scrabble'].length);
+      if(players.length === maxPlayers && io.sockets.adapter.rooms['family-scrabble'].length === maxPlayers){
+        console.log("START GAME");
+        console.log(players);
+      }
+
+    } else {
+      console.log("PLAYERS full: " + players.length);
+      // notify client (only the one)
+      socket.emit('game full', 'Game full.');
+    }
+
+    console.log("SOCKETS at connection end:");
+    console.log(io.sockets.adapter.rooms['family-scrabble']);
+
+    console.log(players);
+    var room = io.sockets.adapter.rooms['family-scrabble'];
+    console.log("ROOM members: " + room.length);
   });
+
 });
 
 // app.listen(3000, function(req, res) {
@@ -241,14 +303,11 @@ count: 1
 }
 ];
 
-const letterStack = [];
-let shuffledLetters = [];
-const players = [];
-const letterStackPerPlayer = 7;
 
-function Player(name, order, letterStack, placement) {
+function Player(name, order, socketID, letterStack, placement) {
   this.name = name;
   this.order = order;
+  this.socketID = socketID;
   this.letterStack = [letterStack];
   this.placement = placement;
 }
@@ -256,7 +315,7 @@ function Player(name, order, letterStack, placement) {
 // console.log(players);
 
 app.get("/", function(req, res){
-  res.render("login");
+  res.render("login", {players: players});
 });
 
 
--- public/js/functions.js
+++ public/js/functions.js
@@ -3,4 +3,35 @@
 $("#playerLogin").on('click', function(event) {
   $("form").addClass("hidden");
   $("#waitPanel").removeClass("hidden");
+
+  var socket = io();
+  $('form').submit(function(e){
+    e.preventDefault(); // prevents page reloading
+    socket.emit('player login', $('#firstName').val());
+    $('#firstName').val('');
+    return false;
+  });
+  socket.on('player login', function(msg){
+$('.players').append($('<p>').text(msg + " logged in."));
+  });
+
+  socket.on('update players', function(players){
+    // redo player list in case new users have arrived since page load
+    $('ul#players').empty();
+    players.forEach(function(player){
+      $('ul#players').append($('<li>').text(player.name));
+    });
+  });
+
+  socket.on('game full', function(msg){
+    $("form").addClass("hidden");
+    $(".loginContainer").empty();
+    $('.players').empty();
+    $('.players').append($('<p>').text(msg));
+  });
+  socket.on('player logout', function(player){
+    $("ul#players li:nth-child(" + (player.index+1) + ")").remove();
+    $('.players').append($('<p>').text(player.name + " logged out."));
+
+  });
 });
