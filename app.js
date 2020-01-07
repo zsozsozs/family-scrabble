b5c820f779364dc696d9c5deda407f53b96ed387
zsozsozs <zso@zso.com>
2020-01-07 10:38:51 +0100

    Change dynamic variable for deployment on heroku

--- app.js
+++ app.js
@@ -36,12 +36,8 @@ const letterStackPerPlayer = 7;
 const maxPlayers = 4;
 let gameOn = false;
 
-// for deployment
-const server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-server.listen(server_port, server_ip_address, function () {
-  // console.log( "Listening on " + server_ip_address + ", port " + server_port )
+server.listen(process.env.port || 3000, function () {
+  console.log("Server running.");
 });
 
 io.on('connection', function(socket) {
