064497cc22e12692006fe365cb3c33f84b61c606
zsozsozs <zso@zso.com>
2020-04-19 19:04:56 +0200

    Decomment letter array for prod

--- shuffle.js
+++ shuffle.js
@@ -20,182 +20,182 @@ const letters = [{
     letter: "c",
     value: 5,
     count: 1
+  },
+  {
+    letter: "cs",
+    value: 7,
+    count: 1
+  },
+  {
+    letter: "d",
+    value: 2,
+    count: 3
+  },
+  {
+    letter: "e",
+    value: 1,
+    count: 6
+  },
+  {
+    letter: "é",
+    value: 3,
+    count: 3
+  },
+  {
+    letter: "f",
+    value: 4,
+    count: 2
+  },
+  {
+    letter: "g",
+    value: 2,
+    count: 3
+  },
+  {
+    letter: "gy",
+    value: 4,
+    count: 2
+  },
+  {
+    letter: "h",
+    value: 3,
+    count: 2
+  },
+  {
+    letter: "i",
+    value: 1,
+    count: 3
+  },
+  {
+    letter: "í",
+    value: 5,
+    count: 1
+  },
+  {
+    letter: "j",
+    value: 4,
+    count: 2
+  },
+  {
+    letter: "k",
+    value: 1,
+    count: 6
+  },
+  {
+    letter: "l",
+    value: 1,
+    count: 4
+  },
+  {
+    letter: "ly",
+    value: 8,
+    count: 1
+  },
+  {
+    letter: "m",
+    value: 1,
+    count: 3
+  },
+  {
+    letter: "n",
+    value: 1,
+    count: 4
+  },
+  {
+    letter: "ny",
+    value: 5,
+    count: 1
+  },
+  {
+    letter: "o",
+    value: 1,
+    count: 3
+  },
+  {
+    letter: "ó",
+    value: 2,
+    count: 3
+  },
+  {
+    letter: "ö",
+    value: 4,
+    count: 2
+  },
+  {
+    letter: "ő",
+    value: 7,
+    count: 1
+  },
+  {
+    letter: "p",
+    value: 4,
+    count: 2
+  },
+  {
+    letter: "r",
+    value: 1,
+    count: 4
+  },
+  {
+    letter: "s",
+    value: 1,
+    count: 3
+  },
+  {
+    letter: "sz",
+    value: 3,
+    count: 2
+  },
+  {
+    letter: "t",
+    value: 1,
+    count: 5
+  },
+  {
+    letter: "ty",
+    value: 10,
+    count: 1
+  },
+  {
+    letter: "u",
+    value: 4,
+    count: 2
+  },
+  {
+    letter: "ú",
+    value: 7,
+    count: 1
+  },
+  {
+    letter: "ü",
+    value: 4,
+    count: 2
+  },
+  {
+    letter: "ű",
+    value: 7,
+    count: 1
+  },
+  {
+    letter: "v",
+    value: 3,
+    count: 2
+  },
+  {
+    letter: "z",
+    value: 4,
+    count: 2
+  },
+  {
+    letter: "zs",
+    value: 8,
+    count: 1
+  },
+  {
+    letter: "",
+    value: 0,
+    count: 2
   }
-  // {
-  //   letter: "cs",
-  //   value: 7,
-  //   count: 1
-  // },
-  // {
-  //   letter: "d",
-  //   value: 2,
-  //   count: 3
-  // },
-  // {
-  //   letter: "e",
-  //   value: 1,
-  //   count: 6
-  // },
-  // {
-  //   letter: "é",
-  //   value: 3,
-  //   count: 3
-  // },
-  // {
-  //   letter: "f",
-  //   value: 4,
-  //   count: 2
-  // },
-  // {
-  //   letter: "g",
-  //   value: 2,
-  //   count: 3
-  // },
-  // {
-  //   letter: "gy",
-  //   value: 4,
-  //   count: 2
-  // },
-  // {
-  //   letter: "h",
-  //   value: 3,
-  //   count: 2
-  // },
-  // {
-  //   letter: "i",
-  //   value: 1,
-  //   count: 3
-  // },
-  // {
-  //   letter: "í",
-  //   value: 5,
-  //   count: 1
-  // },
-  // {
-  //   letter: "j",
-  //   value: 4,
-  //   count: 2
-  // },
-  // {
-  //   letter: "k",
-  //   value: 1,
-  //   count: 6
-  // },
-  // {
-  //   letter: "l",
-  //   value: 1,
-  //   count: 4
-  // },
-  // {
-  //   letter: "ly",
-  //   value: 8,
-  //   count: 1
-  // },
-  // {
-  //   letter: "m",
-  //   value: 1,
-  //   count: 3
-  // },
-  // {
-  //   letter: "n",
-  //   value: 1,
-  //   count: 4
-  // },
-  // {
-  //   letter: "ny",
-  //   value: 5,
-  //   count: 1
-  // },
-  // {
-  //   letter: "o",
-  //   value: 1,
-  //   count: 3
-  // },
-  // {
-  //   letter: "ó",
-  //   value: 2,
-  //   count: 3
-  // },
-  // {
-  //   letter: "ö",
-  //   value: 4,
-  //   count: 2
-  // },
-  // {
-  //   letter: "ő",
-  //   value: 7,
-  //   count: 1
-  // },
-  // {
-  //   letter: "p",
-  //   value: 4,
-  //   count: 2
-  // },
-  // {
-  //   letter: "r",
-  //   value: 1,
-  //   count: 4
-  // },
-  // {
-  //   letter: "s",
-  //   value: 1,
-  //   count: 3
-  // },
-  // {
-  //   letter: "sz",
-  //   value: 3,
-  //   count: 2
-  // },
-  // {
-  //   letter: "t",
-  //   value: 1,
-  //   count: 5
-  // },
-  // {
-  //   letter: "ty",
-  //   value: 10,
-  //   count: 1
-  // },
-  // {
-  //   letter: "u",
-  //   value: 4,
-  //   count: 2
-  // },
-  // {
-  //   letter: "ú",
-  //   value: 7,
-  //   count: 1
-  // },
-  // {
-  //   letter: "ü",
-  //   value: 4,
-  //   count: 2
-  // },
-  // {
-  //   letter: "ű",
-  //   value: 7,
-  //   count: 1
-  // },
-  // {
-  //   letter: "v",
-  //   value: 3,
-  //   count: 2
-  // },
-  // {
-  //   letter: "z",
-  //   value: 4,
-  //   count: 2
-  // },
-  // {
-  //   letter: "zs",
-  //   value: 8,
-  //   count: 1
-  // },
-  // {
-  //   letter: "",
-  //   value: 0,
-  //   count: 2
-  // }
 ];
 
 exports.shuffleAtStart = function(){
